848971f52d24fb642c429143c811d0ed33594c58
rpercas600 <rpercas600@iesmartinezm.es>
2023-02-06 17:56:31 +0100

    navigation casi done

--- app/src/main/java/iesmm/pmdm/socialdrivemm/NavigationDrawer.java
+++ app/src/main/java/iesmm/pmdm/socialdrivemm/NavigationDrawer.java
@@ -1,25 +1,64 @@
 package iesmm.pmdm.socialdrivemm;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.MenuItem;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class NavigationDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_drawer);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_nav,R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+                break;
+            case R.id.nav_about:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new AboutFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new SettingsFragment()).commit();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this,"Logout!",Toast.LENGTH_LONG).show();
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
         return false;
     }
 }
