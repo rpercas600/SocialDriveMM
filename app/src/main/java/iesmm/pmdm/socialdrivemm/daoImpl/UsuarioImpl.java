567911d0f88ff5c4e71d6a4ab31b89fb331667e2
AleGIT14 <aleromcon1407@iesmartinezm.es>
2023-02-13 21:17:10 +0100

    conexion bd

--- app/src/main/java/iesmm/pmdm/socialdrivemm/daoImpl/UsuarioImpl.java
+++ app/src/main/java/iesmm/pmdm/socialdrivemm/daoImpl/UsuarioImpl.java
@@ -4,7 +4,6 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
--- app/src/main/java/iesmm/pmdm/socialdrivemm/sql/socialdrive.sql
+++ app/src/main/java/iesmm/pmdm/socialdrivemm/sql/socialdrive.sql
@@ -1,6 +1,31 @@
-DROP DATABASE IF EXISTS socialdrivemm;
-CREATE DATABASE socialdrivemm CHARACTER SET utf8mb4;
-USE socialdrivemm;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-02-2023 a las 21:16:35
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `socialdrivemm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marcador`
+--
 
 CREATE TABLE `marcador` (
   `id` int(5) NOT NULL,
@@ -9,27 +34,65 @@ CREATE TABLE `marcador` (
   `descripcion` varchar(20) NOT NULL,
   `via` varchar(10) NOT NULL,
   `user` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
 
 CREATE TABLE `usuario` (
   `user` varchar(15) NOT NULL,
   `pass` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
 
 INSERT INTO `usuario` (`user`, `pass`) VALUES
 ('Ale', '1234'),
 ('Rafa', '1234');
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `marcador`
+--
 ALTER TABLE `marcador`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_loggued` (`user`);
 
+--
+-- Indices de la tabla `usuario`
+--
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`user`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `marcador`
+--
 ALTER TABLE `marcador`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `marcador`
+--
 ALTER TABLE `marcador`
   ADD CONSTRAINT `user_loggued` FOREIGN KEY (`user`) REFERENCES `usuario` (`user`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
